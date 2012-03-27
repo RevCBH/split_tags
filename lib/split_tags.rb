0ab062dec08a2aaada42fbeee4e911a6bdd4b1f9
Ben Hoffman <benn.hoffman@gmail.com>
2012-03-27 16:32:18 -0500

    Added on load cleanup of null and blank tags

--- lib/split_tags.rb
+++ lib/split_tags.rb
@@ -10,6 +10,7 @@ module SplitTags
 
 		def before_save(*args)
       doc = args[0] # TODO - should work with just (doc) instead of (args*), etc.
+      puts "split_tags before_save"
 
 			val = doc.send "#{@attribute}"
 			if val.respond_to? :map
@@ -26,12 +27,25 @@ module SplitTags
 
 			doc.send "#{@attribute}=", val
 		end
+
+		def after_find(doc)
+			val = doc.send "#{@attribute}"
+			if val.respond_to? :reject!							
+				val = val.reject {|x| x.nil? || x == ""}
+				doc.send "#{@attribute}=", val
+			end
+		end
+
+		alias_method :after_initialize, :after_find
 	end
 
 	module ClassMethods
 		def split_tags(*args)			
 			args.each do |t|
-				before_save Splitter.new(t.to_s)
+				splitter = Splitter.new(t.to_s)
+				before_save splitter
+				after_find splitter
+				after_initialize splitter
 			end
 		end
 	end
